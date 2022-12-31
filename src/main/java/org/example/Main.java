b922d8a3b16d301c0b35f560a32acd953ee6b95a
Tom Griesbach <Tom.Griesbach@gmx.de>
2022-12-31 20:22:13 +0100

    Main update Die Main Klasse wurde aktualisiert mit der Erstellung der notwendigen Klassen. jetzt Projekt auf dem Stand von damals aus dem ersten Semester.
    Der Code ist aktuell sehr schwer zu lesen, daher muss ein Refactoring vorgenommen werden.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DominoGame dominoGame = new DominoGame();
+        dominoGame.addPlayer(new HumanPlayer());
+        dominoGame.addPlayer(new StupidPlayer());
+        dominoGame.startGame();
     }
 }
